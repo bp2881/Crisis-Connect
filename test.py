bb1df411064b5c510bb4c495a126eb8b19c9ce3a
bp2881 <pranavbairy2@gmail.com>
2024-09-16 23:10:06 +0530

    The errors from the data are filtered subtly
    	new file:   .gitignore
    	modified:   a.json
    	modified:   test.py
    	modified:   train.py

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 from flask import Flask, render_template, request
 import requests
 import json
-from train import train  # Import the updated train function
+from train import train  
 
 app = Flask(__name__)
 
-# Your NewsAPI key
 api_key = '3b77d430512c4f9798369125496c451a'
 
 def dindex():
-    # Define the API endpoint and parameters
     url = 'https://newsapi.org/v2/everything'
     params = {
         'q': 'disaster',
@@ -18,14 +16,12 @@ def dindex():
         'apiKey': api_key
     }
 
-    # Make the API request
     response = requests.get(url, params=params)
     news_data = response.json()
 
     if news_data.get('status') == 'ok':
         articles = news_data['articles']
         
-        # Convert the list of articles to a dictionary
         articles_dict = {index: article for index, article in enumerate(articles)}
         
         with open("a.json", "w+", encoding="utf-8") as h:
@@ -33,14 +29,11 @@ def dindex():
 
 dindex()
 
-# Load the JSON file and process each article
 with open("a.json", "r") as file:
     articles_dict = json.load(file)
-
-    # Iterate over the articles and filter using train function
     for i in range(len(articles_dict)):
         description = articles_dict[f"{i}"]["description"]
         filtered = train(description)
-        
+
         if filtered:
             print(f"Disaster-related article: {filtered[0]}")
--- train.py
+++ train.py
@@ -1,15 +1,39 @@
-disaster_keywords = [
+good_keywords = [
     "disaster", "floods", "flood", "earthquake", "tsunami", 
     "cyclone", "hurricane", "wildfire", "avalanche",
     "drought", "tornado", "landslide"
 ]
 
+error_keywords = [
+    "BJP", "Bharatiya Janata Party", "BSP", "Bahujan Samaj Party",
+    "CPI", "Communist Party of India", "CPM", "Communist Party of India (Marxist)",
+    "INC", "Indian National Congress", "NCP", "Nationalist Congress Party",
+    "AIFB", "All India Forward Bloc", "DMDK", "Desiya Murpokku Dravida Kazhagam",
+    "INLD", "Indian National Lok Dal", "IUML", "Indian Union Muslim League",
+    "JD(U)", "Janata Dal (United)", "JKNPP", "Jammu & Kashmir National Panthers Party",
+    "LJP", "Lok Jan Shakti Party", "RLD", "Rashtriya Lok Dal", "SAD", 
+    "Shiromani Akali Dal", "SHS", "Shivsena", "SP", "Samajwadi Party", 
+    "Aa S P", "Asankhya Samaj Party", "AAAP", "Aam Aadmi Party", 
+    "AACP", "Adarshwaadi Congress Party", "ABHM", "Akhil Bharat Hindu Mahasabha",
+    "ABSPARTY", "Akhil Bhartiya Sudhar Party", "ABVCP", "Akhil Bhartiya Vikas Congress Party",
+    "AIBS", "All India Bahujan Samman Party", "AICP", "New All India Congress Party",
+    "ANC", "Ambedkar National Congress", "AP", "Awami Party", 
+    "ATBP", "Atulya Bharat Party", "AVIRP", "AARAKSHAN VIRODHI PARTY", 
+    "BASMM", "Bahujan Samaj Mukti Morcha", "BASPB", "Bahujan Smajwadi Party (Baba Saheb)"
+]
+
+
 def train(text):
-    filtered_articles = []
+	for word in error_keywords:
+		if word.lower() in text.lower():
+			return []
 
-    for word in disaster_keywords:
-        if word.lower() in text.lower():
-            filtered_articles.append(text)
-            break 
+	filtered_articles = []
 
-    return filtered_articles
+	for word in good_keywords:
+		if word.lower() in text.lower():
+			filtered_articles.append(text)
+			break 
+
+	return filtered_articles
+
